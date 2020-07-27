ab26c9f4477f7b28df9f4cbd308b815f5b3d5986
zainmk <zain.mk@hotmail.com>
2020-07-27 06:23:39 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-
-
-# import calgary
 import data_analysis
+
+
+print("Updating data...")
+import calgary
+import toronto
+
 
 # Running the different kinds of statistical analysis coded in 'data_analysis.py'
 choiceDict = {"1": "calgarytb", "2": "torontotb"}
@@ -12,6 +15,9 @@ print("2. Toronto, ON")
 
 choice = input()
 
+data_analysis.regular_trend(choiceDict[choice])
+
+"""
 data_analysis.data_collect(choiceDict[choice])
 data_analysis.basic_stats_total_cases(choiceDict[choice])
 data_analysis.linear_analysis_days_vs_total_cases(choiceDict[choice])
@@ -22,8 +28,7 @@ data_analysis.linear_analysis_temp_vs_total_cases(choiceDict[choice])
 data_analysis.linear_analysis_temp_vs_new_cases(choiceDict[choice])
 data_analysis.multiple_regression_days_and_temp_vs_total_cases(choiceDict[choice])
 data_analysis.multiple_regression_days_and_temp_vs_new_cases(choiceDict[choice])
-
-
+"""
 
 
 
